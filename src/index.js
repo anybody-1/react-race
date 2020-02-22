dab17cf759489d48bab96eb0c16131c3fa271d16
lvshuangyong <1721446288@qq.com>
2020-02-22 23:14:04 +0800

    测试hook

--- src/index.js
+++ src/index.js
@@ -1,60 +1,17 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { render } from 'react-dom'
-import './index.css'
 // ========================================
-let theme = React.createContext()
-
-function Button(props) {
-  return <button onClick={props.change}>改变主题</button>
-}
-function Box(props) {
-  return <div className={`box ${props.theme}`}>
-    {props.children}
-  </div>
-}
-function Input(props) {
-  return <input type="text" value={props.theme} />
-}
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      color: 'blue',
-      changeColor: () => {
-        if (this.state.color === 'blue') {
-          this.setState({
-            ...this.state,
-            color: 'green'
-          })
-        } else {
-          this.setState({
-            ...this.state,
-            color: 'blue'
-          })
-        }
-
-      }
-    }
+const App = function () {
+  const [n, setN] = useState(0)
+  const add = () => {
+    setN(n + 1)
   }
-  render() {
-    return (
-      <theme.Provider value={this.state}>
-        <theme.Consumer>
-          {theme => (
-            <div>
-              <Box theme={theme.color}>
-                <Button change={theme.changeColor}></Button>
-              </Box>
-              <Box theme={theme.color}>
-                <Input></Input>
-              </Box>
-            </div>
-          )}
-        </theme.Consumer>
-      </theme.Provider>
-    )
-
-  }
+  return (
+    <div>
+      <div>{n}</div>
+      <button onClick={add}>+1</button>
+    </div>
+  )
 }
 render(
   <App />,
